b91520f43cffb49f89745ea7dd9095d034adab3a
Meherab Hasan Fahim <fahim2305341670@diu.edu.bd>
2026-02-16 20:34:22 +0600

    .

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,8 +2,7 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
