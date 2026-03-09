2e60ddbf823d3aaf4f649a6023f98f9d0b43bb9f
Meherab Hasan Fahim <meherabhasan.dev@gmail.com>
2026-03-10 00:14:31 +0600

    Update ScrollToTop.jsx

--- client/src/components/ScrollToTop.jsx
+++ client/src/components/ScrollToTop.jsx
@@ -3,9 +3,12 @@ import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
+
   useEffect(() => {
+    window.history.scrollRestoration = 'manual';
     window.scrollTo(0, 0);
   }, [pathname]);
+
   return null;
 };
 
